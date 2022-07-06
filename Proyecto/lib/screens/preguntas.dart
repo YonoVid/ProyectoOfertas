78060b206d2553ac90644d66bfcc1cb8305483a5
CarolinaCatalan <carolina.catalan14@inacapmail.cl>
2022-07-06 18:49:22 -0400

    Preguntas¿?¿?¿?

--- Proyecto/lib/screens/preguntas.dart
+++ Proyecto/lib/screens/preguntas.dart
@@ -13,9 +13,52 @@ class Preguntas extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          PreguntaCard(question: '¿Te gustan las galletas?', answer: 'No'),
-          PreguntaCard(question: '¿Te gustan las marraquetas?', answer: 'Sipi'),
-          PreguntaCard(question: '¿Son reales estos descuentos tan hermosos?', answer: 'Asies'),
+          PreguntaCard(
+            question: '¿Como Filtrar Ofertas?',
+            answer: 'Debe apretar el simbolo del embudo el cual lo llevará a los filtros. '
+                'Escoja la categoría que desee ver y ajuste el precio que desee. Luego de esto aprete el boton "Aplicar filtros". '
+                'Si desea quitar los filtros seleccionados aprete el botón "Eliminar filtros".',
+            imagen: Image.asset('assets/Filtros.png'),
+          ),
+          PreguntaCard(
+            question: '¿Como Buscar Ofertas?',
+            answer: 'Para realizar la búsqueda de un producto en oferta debe presionar en "Buscar término", luego ingrese el nombre del producto '
+                'que desea buscar y este aparecera en la pantalla. Presione la "X" para eliminar la búsqueda y ver todas las ofertas',
+            imagen: Image.asset('assets/Buscador.png'),
+          ),
+          PreguntaCard(
+            question: '¿Como Guardar una Oferta?',
+            answer: 'Para guardar una oferta debe apretar el botón en forma de corazón que se encuentra al lado derecho de la oferta. Una vez que '
+                'el botón cambie de color a rojo significa que ha quedado guardada.',
+            imagen: Image.asset('assets/Guardar_Preferencias.png'),
+          ),
+          PreguntaCard(
+            question: '¿Como Ver Ofertas Guardadas?',
+            answer: ' Para acceder a sus preferencias puede seleccionar el corazón en la parte superior de la pantalla Ofertas'
+                ' o desde el menú ir a "Ofertas Guardadas".',
+            imagen: Image.asset('assets/Encontrar_Ofertas.png'),
+          ),
+          PreguntaCard(
+            question: '¿Como Reportar una Oferta Sospechosa?',
+            answer: ' Si usted ve una oferta sospechosa debe presionar el botón de exclamación que esta a la derecha.'
+                'Escoja la razón del reporte junto con su comentario del porque le resulta sospechosa.',
+            imagen: Image.asset('assets/Reportar.png'),
+          ),
+          PreguntaCard(
+              question: '¿Te gustan las galletas?',
+              answer: 'No',
+            imagen: Image.asset('assets/Galletas.PNG'),
+          ),
+          PreguntaCard(
+            question: '¿Te gustan las marraquetas?',
+            answer: 'Sipi',
+            imagen: Image.asset('assets/Marraqueta.PNG'),
+          ),
+          PreguntaCard(
+            question: '¿Son reales estos descuentos tan hermosos?',
+            answer: 'Asies',
+            imagen: Image.asset('assets/Ofertas.PNG'),
+          ),
         ],
       ),
       drawer: NavDrawer(
@@ -27,16 +70,17 @@ class Preguntas extends StatelessWidget {
 }
 
 class PreguntaCard extends StatelessWidget {
-  const PreguntaCard({Key? key, required this.question, required this.answer})
+  const PreguntaCard({Key? key, required this.question, required this.answer, required this.imagen})
       : super(key: key);
 
   final String question;
   final String answer;
+  final Image imagen;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+      margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
       color: Colors.brown[50],
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
@@ -53,11 +97,14 @@ class PreguntaCard extends StatelessWidget {
             Padding(
                 padding: EdgeInsets.all(20.0),
                 child: Text(
-                    answer,
-                    style: TextStyle(
+                  answer,
+                  style: TextStyle(
                       fontSize: 16
-                    ),
+                  ),
                 )),
+            Container(
+              child: imagen,
+            ),
           ],
         ),
       ),
